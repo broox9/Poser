bdb9b22d9ec8b3be92fc12e928985a3a1953ca5b
Brookes Stephens <broox9@gmail.com>
2015-03-30 16:40:48 -0400

    ITS WORKING

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 /* = DB Connect */
 mongoose.connect('mongodb://localhost/calendar');
@@ -23,7 +24,7 @@ var postEventSchema = {
 	created_by: String,
 	pid: Number,
 	post_ids:[String],
-	posts: Array,
+	posts: [],
 	resource_name: String,
 	stream_name: String,
 	state: String,
@@ -56,33 +57,65 @@ app.get('/state_counts', function (req, res) {
   console.log("~~State Counts Request~~")
   StateCount.find(function (err, docs) {
 	  res.send(docs);
-	  console.log("STATE_COUNT", !!docs)
   });
 });
 
+
 //multiple callbacks just pipe the stream result
 app.post('/state_counts', jsonParser, function (req, res) {
-  console.log("~~State Counts Post~~", req.body);
-  var sc = StateCount.findOne({"_id" :req.body._id} , function (err, doc) {
-  	if (err) {
-      console.log("<<< %s", err.toString());
-      res.sendStatus(500);
+  var request = req.body;
+  console.log("~~State Counts Post~~", request);
+
+  StateCount.findOne({event_time: request.event_time, state: request.state }, function (err, doc) {
+    if (err) {
+      res.status(500).send(err);
       return;
     }
-    console.log("CallBack DOC", doc);
-	  res.sendStatus(200)
+
+    doc.start = doc.event_time = doc.exact_start = request.start;
+    doc.save(function (err) {
+      if (!err) {
+        setTimeout(function () {res.status(200).json(doc); }, 3500)
+        return;
+      }
+	  res.status(500).json(err)
+    });
   });
+
 });
 
 
 
 app.get('/post_events', function (req, res) {
-  console.log("~~Post Events Request~~")
+  console.log("~~Post Events Request~~");
   PostEvent.find(function (err, docs) {
-	  res.send(docs);
-	  console.log("POST_EVENT", !!docs)
+	 res.send(docs);
   });
 });
+
+
+app.post('/post_events', jsonParser, function (req, res) {
+  var request = req.body;
+  console.log("~~Post Events Request~~");
+
+  PostEvent.findOne({pid: request.pid}).exec(function (err, doc) {
+    if (err) {
+      console.log("<<< %s", err.toString());
+      res.sendStatus(500);
+      return;
+    }
+
+    doc.exact_start = doc.event_time = doc.start = request.start;
+    doc.save(function (err) {
+      if (err) {
+        res.status(500).jsonp(err);
+        return;
+      }
+      res.status(200).jsonp( doc ).end();
+    });
+  });
+});
+
 
 
 /* Server */
